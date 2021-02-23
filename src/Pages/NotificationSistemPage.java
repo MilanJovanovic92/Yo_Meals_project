256248b5b19c077308e0245c18e062c521fcfaee
Milan Jovanovic <milan92ng@gmail.com>
2021-02-23 19:14:35 +0100

    Notification page finish

--- src/Pages/NotificationSistemPage.java
+++ src/Pages/NotificationSistemPage.java
@@ -1,5 +1,31 @@
 package Pages;
 
-public class NotificationSistemPage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class NotificationSistemPage extends BasicPage {
+
+	public NotificationSistemPage(WebDriver driver, WebDriverWait wait, JavascriptExecutor js) {
+		super(driver, wait, js);
+	}
+
+	public WebElement getElementMessage() {
+		return driver.findElement(By.xpath(
+				"//*[contains(@class, 'alert--success') or contains(@class, 'alert--danger')][contains(@style,'display: block')]"));
+
+	}
+
+	public String getMessage() {
+		return this.getElementMessage().getText();
+	}
+
+	public void notificationInvisible() {
+		WebElement systemMessage = driver.findElement(By.xpath("//*[contains(@class, 'system_message')]"));
+		wait.until(ExpectedConditions.attributeToBe(systemMessage, "style", "display: none;"));
+	}
 
 }
